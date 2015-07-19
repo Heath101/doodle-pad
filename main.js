5d02dbe73094fdebc5cedf55e6dc7a008d83432e
Heath Attig <heath.attig@gmail.com>
2015-07-18 21:34:54 -0500

    draw circle around mouse on movement

--- main.js
+++ main.js
@@ -6,23 +6,34 @@ function getMousePos(canvas, evt) {
   };
 }
 
+window.requestAnimFrame = (function(callback) {
+  return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+  function(callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
+})();
+
 var statsEl = document.getElementById('stats');
 
 var canvas = document.getElementById('drawingArea');
 var context = canvas.getContext('2d');
-var centerX = canvas.width / 2;
-var centerY = canvas.height / 2;
 var radius = 70;
 
-context.beginPath();
-context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-context.fillStyle = 'green';
-context.fill();
-context.lineWidth = 5;
-context.strokeStyle = '#003300';
-context.stroke();
+
+function drawShape(x, y, context) {
+  context.beginPath();
+  context.arc(x, y, radius, 0, 2 * Math.PI, false);
+  context.fillStyle = 'green';
+  context.fill();
+  context.lineWidth = 5;
+  context.strokeStyle = '#003300';
+  context.stroke();
+}
+
+
 
 canvas.addEventListener('mousemove', function(evt) {
   var mousePos = getMousePos(canvas, evt);
   statsEl.innerHTML = mousePos.x + ',' + mousePos.y;
+  drawShape(mousePos.x, mousePos.y, context)
 }, false);

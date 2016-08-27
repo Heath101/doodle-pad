8652e821cf8c6b7800bb12fc2db7f972a0f39b64
Heath Attig <heath.attig@gmail.com>
2016-08-27 08:16:16 -0500

    add a few features

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ var context  = canvas.getContext('2d');
 
 var lastMousePosX
 var lastMousePosY
-var radius = 50;
+var radius = 40;
 var currentColor = '#00aaff'
 
 
@@ -34,24 +34,48 @@ function drawShape(x, y, context) {
   context.stroke();
 }
 
+function drawBrush(x, y, context) {
+  // context.arc(x,y, radius, 0, 2 * Math.PI, false);
+  // context.stroke();
+}
+
 canvas.addEventListener('mousemove', function(evt) {
   var mousePos = getMousePos(canvas, evt);
   statsEl.innerHTML = mousePos.x + ',' + mousePos.y;
+
   if (evt.which == 1) {
     drawShape(mousePos.x, mousePos.y, context)
+  } else {
+    drawBrush(mousePos.x, mousePos.y, context)
   }
+
   lastMousePosX = mousePos.x
   lastMousePosY = mousePos.y
 
 }, false);
 
-document.getElementById('increaseRadius').addEventListener('click', function() { 
+// Size Adjust
+document.getElementById('increaseRadius').addEventListener('click', function() {
   radius += 5
   radiusEl.innerHTML = radius
 })
-document.getElementById('decreaseRadius').addEventListener('click', function() { 
+document.getElementById('decreaseRadius').addEventListener('click', function() {
   if (radius > 5) {
     radius -= 5
     radiusEl.innerHTML = radius
   }
 })
+
+// Clear screen
+document.getElementById('clearScreen').addEventListener('click', function() {
+  context.clearRect(0,0,1200,800)
+})
+
+// Color Boxes
+var boxes = document.getElementsByClassName('color-box')
+for(i=0; i < boxes.length; i++) {
+  var colorBox = boxes[i]
+  colorBox.addEventListener('click', function(e) {
+    currentColor = this.getAttribute('data-color')
+  })
+}
